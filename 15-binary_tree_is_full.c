5059f975e77ac01d4ab4e8c2f0ba0c4c7039f9d3
Benita Michael <benubiaka@gmail.com>
2023-10-30 23:27:27 -0700

    mandatory tasks

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * is_full_recursive - function to check if a trr is full recursively
+ * @tree: pointer to the root node of the tree to check
+ * Return: If tree is not full, return (0), else return (1)
+ */
+int is_full_recursive(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_recursive(tree->left) == 0 ||
+		    is_full_recursive(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - function that checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ * Return: If tree is NULL or is not full returns (0), else returns (1)
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_full_recursive(tree));
+}
--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 #### Group project
 #### Algorithm
 #### Data structure
+
+# Binary Trees:
+Binary trees are a data structure in which a record is linked to two successor records, usually referred to as the left branch when greater and the right when less than the previous record.
+It is a data structure in which each node has at most two children, referred to as the left child and the right child.
+
+## Difference Between Binary Trees and Binary Search Tree
+A Binary Tree is a basic structure with a simple rule that no parent must have more than 2 children whereas the Binary Search Tree is a variant of the binary tree following a particular order with which the nodes should be organized.
+
+#### This project was carried out by Enubiaka Benita
